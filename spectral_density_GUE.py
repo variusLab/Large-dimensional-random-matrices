da7bd8841123e17b59a00f8862cc82360b1a682d
Varvara Petrova <150529315+variusLab@users.noreply.github.com>
2024-01-31 14:42:02 +0100

    Update spectral_density_GUE.py

--- spectral_density_GUE.py
+++ spectral_density_GUE.py
@@ -20,7 +20,7 @@ m = 20 # taille de la matrice
 evals = np.array([]) # contiendra N*m elements (valeurs propres des matrices aleatoires de taille mxm sur N tirages)
 for i in range(N):
     # cas GUE    
-    g = np.random.normal(0, 1./np.sqrt(2.), (n,n))+1j*np.random.normal(0, 1./np.sqrt(2.), (n,n))
+    g = np.random.normal(0, 1./np.sqrt(2.), (m,m))+1j*np.random.normal(0, 1./np.sqrt(2.), (m,m))
     h = (g + dag(g))/np.sqrt(2.) # h matrice hermitienne
     # cas GOE
     #g = np.random.normal(0,1,(m,m)) 
